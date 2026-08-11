af8c7dcedc764853ec3d786b22f6df1a219e385b
JEMEETSHAHI9 <jemeetshahi721@gmail.com>
2026-08-11 18:17:02 +0530

    Remove Ananya Deshmukh from team section and align grid to 3 members

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -38,7 +38,7 @@ export function TeamSection() {
         </div>
 
         {/* Team Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
           {teamList.map((member) => (
             <div
               key={member.id}
